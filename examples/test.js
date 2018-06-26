d9a64d5054d4422acd97e2f52a1faa173b5ab975
Adem Kouki <ademkingnew@gmail.com>
2018-06-26 18:54:36 +0100

    Add files via upload

---
+++ examples/test.js
@@ -0,0 +1,38 @@
+cc("Hello ",bg_red, medium, white," World", bg_black, white, medium )
+cc_custom("Test Custom" , "custom-console");
+cc_primary("Test Primary");
+cc_success("Test Success");
+cc_info("Test info");
+cc_warning("Test Warning");
+cc_danger("Test Danger");
+cc_color("Test color" , "red");
+cc_zebre("Test Zebre");
+cc_rainbow("Rainbow Text");
+cc_bold("hello bold");
+cc_italic("hello italic");
+cc_midline("Midline Text");
+cc_underline("Underline Text");
+cc_xsmall("X-Small Text");
+cc_small("Small Text");
+cc_medium("Medium Text");
+cc_large("Large Text");
+cc_xlarge("X-Large Text");
+cc("CoolConsole", bold, red, large, " is the Coolest 😎", medium);
+cc_icon(icon_facebook, "Follow Me On Facebook : https://facebook.com/AdemKouki.Officiel");
+cc_icon(icon_github, "Follow Me On GitHub : https://github.com/Ademking");
+cc_icon(icon_youtube, "Follow Me On Youtube : https://youtube.com/Adamos");
+cc_success("Request Sent Successfully");
+cc_rainbow("RAINBOWWWWWWWWWW");
+
+cc_zebre("Black & White");
+cc_icon(icon_diamond, "Diamonds Baby!")
+cc_icon(icon_hand, "High-5");
+cc_line("blue");
+cc_line("red");
+cc_line("green");
+cc_line("yellow");
+cc_line("orange");
+cc_line("indigo");
+cc_icon(icon_flower, "Roses Are red!, Violets Are Blue");
+cc_icon(icon_thumb_up, "Like Us On Facebook")
+cc_icon(icon_facebook, "https://facebook.com/Ademkouki.Officiel", "Facebook");
